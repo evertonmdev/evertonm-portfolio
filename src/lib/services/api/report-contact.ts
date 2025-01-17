b31a9d2a338e022088069e80a2f8d7a57050dea6
Everton Matheus <mgtk.me@gmail.com>
2025-01-17 00:01:45 -0300

    ...

--- src/lib/services/api/report-contact.ts
+++ src/lib/services/api/report-contact.ts
@@ -2,20 +2,20 @@ import { Messenger } from "@/lib/modules/messenger";
 import { telegramSendMessage } from "./telegram-send-message";
 
 export const reportContact: Messenger["reportContact"] = async ({
-	message,
-	name,
-	email,
+  message,
+  name,
+  email,
 }) => {
-	try {
-		await telegramSendMessage(`Olá Everton!, ${name} entrou em contato com você atraves do seu portfolio.
+  try {
+    await telegramSendMessage(`Olá Everton!, ${name} entrou em contato com você atraves do seu portfolio.
         A mensagem deixada foi: 
         ${message}
         
         Email: ${email}
         `);
-		return true;
-	} catch (e) {
-		console.log(e);
-		return false;
-	}
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 };
